156f21cbe726566880d0b817fa103c35d6b21192
Rafael <60444588+rafaelgonzales612@users.noreply.github.com>
2025-06-01 15:16:01 -0500

    Add files via upload

--- SQL Project - Exploratory Data Analysis.sql
+++ SQL Project - Exploratory Data Analysis.sql
@@ -3,49 +3,73 @@
 -- https://www.kaggle.com/datasets/swaptr/layoffs-2022
 
 SELECT *
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
+-- query for percentage laid off to see how big the layoffs were
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
-FROM layoffs_staging2;
+FROM world_layoffs.layoffs_staging2;
 
+-- query to show which companies laid off 100% of their employees (denoted by 1 in percentage_laid_off)
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
+WHERE percentage_laid_off = 1;
+
+-- same as above query but ordering by funds raised to see how big companies were
+SELECT *
+FROM world_layoffs.layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
-SELECT company, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
+-- companies with the biggest layoff in a single day
+SELECT company, total_laid_off
+FROM world_layoffs.layoffs_staging
+ORDER BY 2 DESC
+LIMIT 10;
 
-SELECT MIN(`date`), MAX(`date`)
-FROM layoffs_staging2;
+-- companies with the most total layoffs
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- most total layoffs by other columns
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
 
 SELECT industry, SUM(total_laid_off)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 GROUP BY industry
-ORDER BY 2 DESC;
+ORDER BY 2 DESC
+LIMIT 10;
 
 SELECT country, SUM(total_laid_off)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 GROUP BY country
-ORDER BY 2 DESC;
+ORDER BY 2 DESC
+LIMIT 10;
 
+-- total layoffs by year
 SELECT YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;
 
+-- total layoffs by month
 SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) AS sum_total_laid_off
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `month`
 ORDER BY 1;
 
+-- rolling total of layoffs per month
 WITH Rolling_Total AS
 (
 SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) sum_total
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `month`
 ORDER BY 1
@@ -54,15 +78,16 @@ SELECT `month`, sum_total,
 SUM(sum_total) OVER(ORDER BY `month`) AS rolling_total
 FROM Rolling_Total;
 
+-- companies with the most layoffs per year
 SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ORDER BY 3 DESC;
 
 WITH Company_Year (company, years, total_laid_off) AS
 (
 SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ), Company_Year_Rank AS
 (
@@ -72,24 +97,5 @@ WHERE years IS NOT NULL
 )
 SELECT *
 FROM Company_Year_Rank
-WHERE Ranking <= 5
-;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+WHERE Ranking <= 5;
 
